bde484d9514c30df9d0b04c449d5b3d1b142eea2
abhay04coder <abhayworldmails@gmail.com>
2024-05-30 22:22:02 +0530

    Second Commit

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext>options):base(options)
+        {
+       
+        }
+       
+            
+        public DbSet<Region>Regions { get; set; }   
+
+        public DbSet<Walks> Walks { get; set; } 
+
+        public DbSet<WalkDifficulty> WalksDifficulties { get; set; }
+
+    }
+    
+    
+}
